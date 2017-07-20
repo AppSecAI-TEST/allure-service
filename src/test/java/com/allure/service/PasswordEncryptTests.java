0836dabbcd202802f5698b485be44b94bdd69b2a
yangshoulai <412837184@qq.com>
2017-07-21 00:07:43 +0800

    update token expiration seconds and some tests

---
+++ src/test/java/com/allure/service/PasswordEncryptTests.java
@@ -0,0 +1,23 @@
+package com.allure.service;
+
+import com.allure.service.component.PasswordEncrypt;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by yann on 2017/7/20.
+ */
+@Slf4j
+public class PasswordEncryptTests {
+
+    private PasswordEncrypt passwordEncrypt = new PasswordEncrypt();
+
+
+    @Test
+    public void encrypt() {
+        String encode = passwordEncrypt.encode("123456");
+        log.debug(encode);
+        Assert.assertTrue(passwordEncrypt.matches("123456", encode));
+    }
+}
